a66c96e00ca1cb54e2be50662633199f477263c0
ohriman <dudgh5184@naver.com>
2020-10-10 22:45:32 +0900

    ~#2.3 Update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Food = ({ name, picture }) => {
-  return <div>
-    <h2>I Like {name}</h2>
-    <img src={picture} />
-  </div> 
+  return (
+    <div>
+      <h2>I Like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
 };
 
-const foodILike = [{
-  name: "Samgyeopsal",
-  image: "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg"
-},
-{
-  name: "Kimchi",
-  image: "https://m.jnmall.kr/web/product/big/201910/4b83072de272a51edffa420ab3b2fa98.jpg"
-}
-]
+const foodILike = [
+  {
+    id: 1,
+    name: "Samgyeopsal",
+    image:
+      "https://pds.joins.com/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg",
+  },
+  {
+    id: 2,
+    name: "Kimchi",
+    image:
+      "https://m.jnmall.kr/web/product/big/201910/4b83072de272a51edffa420ab3b2fa98.jpg",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
-};
+}
 
 export default App;
